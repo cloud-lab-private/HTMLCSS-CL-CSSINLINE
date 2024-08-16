de92e0ac7e29d6234debd64bbfdfc42603c20214
Dilip Babu <106616107+619DILIP@users.noreply.github.com>
2024-08-16 14:48:21 -0400

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -22,7 +22,7 @@ public class SeleniumTest {
         System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
 
         // Get file
-        File file = new File("StyledElements.html");
+        File file = new File("src/main/StyledElements.html");
         String path = "file://" + file.getAbsolutePath();
 
         // Create a new ChromeDriver instance
